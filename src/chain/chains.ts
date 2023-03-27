16f217fd735abb16e6ce6122f9336ff842e57342
RauppRafael <raupperino@gmail.com>
2023-03-27 19:07:49 -0300

    feat: added parseChain helper

--- src/chain/chains.ts
+++ src/chain/chains.ts
@@ -1,3 +1,5 @@
+import { Web3Errors } from '../classes/WalletProvider'
+
 export enum Chain {
     ETH = 1,
 
@@ -14,3 +16,13 @@ export enum Chain {
 export const allChains: Chain[] = Object.keys(Chain)
     .map(n => Number.parseInt(n))
     .filter(n => !Number.isNaN(n))
+
+export const parseChain = (chain: string | number): Chain => {
+    if (typeof chain === 'string')
+        chain = parseInt(chain)
+
+    if (Object.values(Chain).includes(chain))
+        return chain as Chain
+
+    throw new Error(Web3Errors.UNSUPPORTED_CHAIN)
+}
