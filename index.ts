b51ec01da23a996fad3ea13f57da9ed78285eefe
RauppRafael <raupperino@gmail.com>
2023-05-27 12:48:38 -0300

    chore: exporting new classes

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 export * from './src/chain'
 
-export * from './src/classes/LocalStorage'
+export * from './src/classes/CircularArray'
 export * from './src/classes/Ethereum'
+export * from './src/classes/LocalStorage'
+export * from './src/classes/MultiRpcProvider'
 export * from './src/classes/WalletProvider'
 
 export * from './src/helpers'
