d0615eabd6c66cd4e9597b9d490e43154081ecec
RauppRafael <raupperino@gmail.com>
2023-05-26 21:01:16 -0300

    chore: removed bignumber import

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 export * from './src/chain'
 
-export * from './src/classes/BigNumberJS'
 export * from './src/classes/LocalStorage'
 export * from './src/classes/Ethereum'
 export * from './src/classes/WalletProvider'
