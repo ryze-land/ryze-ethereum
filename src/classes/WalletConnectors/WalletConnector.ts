8050b837919654a0d283962d4859e98ebe265191
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-08-15 10:51:24 -0300

    feat: create abstract class `WalletConnector`

---
+++ src/classes/WalletConnectors/WalletConnector.ts
@@ -0,0 +1,18 @@
+/**
+ * An abstract class that provides the interface to implement a Wallet Connector.
+ */
+export abstract class WalletConnector<Provider = any> {
+    /** Unique wallet connector id */
+    public abstract readonly id: string
+    /** Wallet connector name */
+    public abstract readonly name: string
+
+    /**
+     * Gets the required provider to interact with the user Wallet.
+     *
+     * The provider should follow the same API as [EIP-1193](https://eips.ethereum.org/EIPS/eip-1193)
+     *
+     * @returns {Provider | Promise<Provider>}
+     */
+    public abstract getProvider(): Provider | Promise<Provider>
+}
---
+++ src/classes/WalletConnectors/index.ts
@@ -0,0 +1,2 @@
+export { WalletConnector } from './WalletConnector'
+
