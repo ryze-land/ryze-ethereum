89c9163a049529bb97ae3b04f304091c956fe0ea
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-08-22 15:05:42 -0300

    chore: export modules

--- src/classes/WalletConnectors/index.ts
+++ src/classes/WalletConnectors/index.ts
@@ -2,3 +2,9 @@ export { WalletConnector } from './WalletConnector'
 
 export { BrowserConnector } from './BrowserConnector'
 export type { WindowProvider } from './BrowserConnector'
+
+export { BinanceWallet } from './BinanceWallet'
+export { BitgetWallet } from './BitgetWallet'
+export { MetaMaskWallet } from './Metamask'
+export { SafePalWallet } from './SafePal'
+export { TrustWallet } from './TrustWallet'
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,5 +1,6 @@
 export * from './WalletInfo'
 export * from './WalletManager'
+export * from './WalletConnectors'
 export * from './Chain'
 export * from './CircularArray'
 export * from './Ethereum'
