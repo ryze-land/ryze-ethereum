39b014a9fd65c8a5c5b3038d280b08270595d4ba
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-08-23 12:02:42 -0300

    chore: make event listeners optional

--- src/classes/WalletManager/eip1193Provider.ts
+++ src/classes/WalletManager/eip1193Provider.ts
@@ -1,11 +1,11 @@
 export interface EIP1193Provider {
     on(eventName: string | symbol, listener: (...args: any[]) => void): this
 
-    addListener(eventName: string | symbol, listener: (...args: any[]) => void): this
+    addListener?(eventName: string | symbol, listener: (...args: any[]) => void): this
 
-    removeListener(eventName: string | symbol, listener: (...args: any[]) => void): this
+    removeListener?(eventName: string | symbol, listener: (...args: any[]) => void): this
 
-    removeAllListeners(event?: string | symbol): this
+    removeAllListeners?(event?: string | symbol): this
 
     request<T>(args: { method: string, params?: unknown[] | Record<string, unknown> }): Promise<T>
 }
