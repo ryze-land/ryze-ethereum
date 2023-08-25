07ea70aa5ad19800b72a74ca952657fb0d85b6e0
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-08-25 18:15:39 -0300

    feat: add abstract methods to to set and add chains

--- src/classes/WalletConnectors/WalletConnector.ts
+++ src/classes/WalletConnectors/WalletConnector.ts
@@ -1,4 +1,8 @@
+import { BrowserProvider } from 'ethers'
+import { ChainId } from '../../enums'
 import { EIP1193Provider } from '../WalletManager/eip1193Provider'
+import { chainRegistry } from '../../assets'
+import { numberToHex } from '../../helpers'
 
 /**
  * An abstract class that provides the interface to implement a Wallet Connector.
@@ -23,4 +27,39 @@ export abstract class WalletConnector<T extends EIP1193Provider = EIP1193Provide
      * @returns {T | undefined | Promise<T | undefined>}
      */
     public abstract getProvider(): T | undefined | Promise<T | undefined>
+
+    /**
+     * Sets the current chain ID.
+     *
+     * @param {BrowserProvider} provider The ethereum provider.
+     * @param {number} chainId The chain ID to set.
+     */
+    public abstract setChain(chainId: ChainId, provider?: BrowserProvider): Promise<void>
+
+    /**
+     * Requests the provider to add the chain ID.
+     *
+     * @param {BrowserProvider} provider The ethereum provider.
+     * @param {number} chainId The chain ID to set.
+     */
+    public abstract addChain(chainId: ChainId, provider?: BrowserProvider): Promise<void>
+
+    public request(provider: BrowserProvider, method: string, params?: any[] | Record<string, any>): Promise<any> {
+        return provider.send(method, params || [])
+    }
+
+    protected _getAddChainParams(chainId: ChainId) {
+        const chainInfo = chainRegistry[chainId]
+
+        return {
+            chainId: numberToHex(chainInfo.id),
+            chainName: chainInfo.name,
+            nativeCurrency: {
+                ...chainInfo.currency,
+                decimals: 18,
+            },
+            rpcUrls: chainInfo.rpcList,
+            blockExplorerUrls: [chainInfo.explorer],
+        }
+    }
 }
