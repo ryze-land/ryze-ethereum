d8100b7ad0f3a8b000f8431cdea5a70af9cefa77
RauppRafael <raupperino@gmail.com>
2023-09-15 16:02:38 -0300

    feat: added helper to parse custom error

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
+export * from './is-mobile'
 export * from './map-n'
 export * from './not-empty'
 export * from './number-to-hex'
+export * from './parse-custom-error'
 export * from './sleep'
-export * from './is-mobile'
