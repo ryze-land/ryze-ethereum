8c1bc3b8114527417134ebdeca4a12621080cd41
RauppRafael <raupperino@gmail.com>
2024-02-08 09:47:20 -0300

    feat: split mainnets and testnets

--- src/constants/ChainId.ts
+++ src/constants/ChainId.ts
@@ -1,29 +1,36 @@
 import { literal as zodLiteral, union as zodUnion, type ZodLiteral } from 'zod'
 
-export const ChainIds = {
+export const Mainnets = {
     ETH: 1,
-
     BNB: 56,
-    BNB_TESTNET: 97,
-
     AVAX: 43114,
-    AVAX_TESTNET: 43113,
-
     POLYGON: 137,
-    POLYGON_TESTNET: 80001,
-
     FANTOM: 250,
-    FANTOM_TESTNET: 4002,
-
     OPTIMISM: 10,
-    OPTIMISM_TESTNET: 420,
-
     ARBITRUM: 42161,
+} as const
+
+export type MainnetName = keyof typeof Mainnets
+
+export const Testnets = {
+    BNB_TESTNET: 97,
+    AVAX_TESTNET: 43113,
+    POLYGON_TESTNET: 80001,
+    FANTOM_TESTNET: 4002,
+    OPTIMISM_TESTNET: 420,
     ARBITRUM_TESTNET: 421613,
+} as const
+
+export type TestnetName = keyof typeof Testnets
+
+export const ChainIds = {
+    ...Mainnets,
+    ...Testnets,
 } as const
 
 export type ChainName = keyof typeof ChainIds
 export type ChainId = typeof ChainIds[ChainName]
+
 export type NonEmptyZodLiterals<T> = [
     ZodLiteral<T>,
     ZodLiteral<T>,
--- src/helpers/hardhat.ts
+++ src/helpers/hardhat.ts
@@ -1,17 +1,18 @@
 import { chainRegistry } from '../assets'
-import { ChainIds, ChainName } from '../constants'
+import { ChainIds, ChainName, MainnetName } from '../constants'
 
 export function getNetworkConfig(
     chainName: ChainName,
-    explorerKeys?: Partial<Record<ChainName, string>>,
+    explorerKeys?: Partial<Record<MainnetName, string>>,
 ) {
     const chain = chainRegistry[ChainIds[chainName]]
+    const mainnetName = chainName.replace('_TESTNET', '') as MainnetName
 
     return {
         name: chainName,
         url: chain.rpc,
         chainId: chain.id,
-        explorer: explorerKeys?.[chainName],
+        explorer: explorerKeys?.[mainnetName],
         gasMultiplier: 2,
     }
 }
