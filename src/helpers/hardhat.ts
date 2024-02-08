3e215857564b07e3200941eb49f275f9968a14a9
RauppRafael <raupperino@gmail.com>
2024-02-08 08:59:32 -0300

    feat: added getNetworkConfig for hardhat

---
+++ src/helpers/hardhat.ts
@@ -0,0 +1,15 @@
+import { chainRegistry } from '../assets'
+import { ChainIds, ChainName, chainNameSchema } from '../constants'
+
+export function getNetworkConfig(
+    chainName: ChainName,
+    explorerKeys: Partial<Record<ChainName, string>>,
+) {
+    const chain = chainRegistry[ChainIds[chainNameSchema.parse(chainName)]]
+
+    return {
+        rpc: chain.rpc,
+        chainId: chain.id,
+        explorer: explorerKeys[chainName],
+    }
+}
