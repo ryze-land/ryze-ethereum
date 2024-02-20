dfde284338e1549faf3a2dd5735795c4be3e2b7d
RauppRafael <raupperino@gmail.com>
2024-02-19 21:07:00 -0300

    feat: added Promisable type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,10 @@
 export type Address = `0x${ string }`
+
 export type Hash = `0x${ string }`
+
+/**
+ * A utility type that represents a value that might be a promise or a direct value.
+ *
+ * @template T The type of the value or the resolved value of the promise.
+ */
+export type Promisable<T> = Promise<T> | T
