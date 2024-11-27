1318ec2b443e771f9c553de09c704291d2184ddb
RauppRafael <raupperino@gmail.com>
2024-11-27 11:41:35 -0300

    feat: added generic type

--- src/classes/WalletConnectors/WalletConnector__disconnect.ts
+++ src/classes/WalletConnectors/WalletConnector__disconnect.ts
@@ -1,5 +1,6 @@
+import { EIP1193Provider } from '../WalletManager/eip1193Provider'
 import { WalletConnector } from './WalletConnector'
 
-export abstract class WalletConnector__disconnect extends WalletConnector {
+export abstract class WalletConnector__disconnect<T extends EIP1193Provider = EIP1193Provider> extends WalletConnector<T> {
     public abstract disconnect(): Promise<void>
 }
